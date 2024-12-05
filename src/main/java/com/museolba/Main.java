948c3ae4003f7776a1f322fd35d4e56853c63a31
JuanNew19 <juanma.a0190@gmail.com>
2024-12-05 09:37:20 -0300

    refactor: Se mejora el inicio del sistema

--- src/main/java/com/museolba/Main.java
+++ src/main/java/com/museolba/Main.java
@@ -1,9 +1,26 @@
 package com.museolba;
 
+import com.formdev.flatlaf.intellijthemes.FlatDraculaIJTheme;
+import com.museolba.vista.ventanaLogin.VentanaLogin;
+import com.museolba.vista.ventanaPrincipal.VentanaPrincipal;
+import javax.swing.UIManager;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         // Configurar FlatLaf como Look and Feel
+        try {
+            UIManager.put( "Button.arc", 999 );
+            UIManager.put( "Component.arc", 999 );
+          //  UIManager.put( "TextComponent.arc", 999 );
+            UIManager.put( "ScrollBar.showButtons", true );
+            UIManager.setLookAndFeel(new FlatDraculaIJTheme());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+        
+        VentanaPrincipal vl = new VentanaPrincipal();
+        vl.setVisible(true);
     }
 }
